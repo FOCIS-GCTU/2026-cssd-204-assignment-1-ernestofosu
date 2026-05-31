0b6b5c4f04f79ffaccaadc11cb6d0f13c1b022f5
ernestofosu <2425404392@live.gctu.edu.gh>
2026-05-31 09:31:34 +0000

    Complete final assignment requirements

--- initials.py
+++ initials.py
@@ -1,7 +1,34 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Ernest Bernard Ofosu
+# STUDENT ID: 2425404392
+# Email: 2425404392@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Ernest Bernard Ofosu , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+   # Main function prints initials in large form; each letter would be printed large using the same letter
 
+
+    print()
+    print("...EBO")
+    print()
+    print("...EEEEEEEEEEEE........BBBBBBBBBBBB........OOOOOOOOOOOO.....")
+    print("...EEEEEEEEEEEE........BBBBBBBBBBBB........OOOOOOOOOOOO.....")
+    print("...EEEE................BBB......BBB........OO0......OOO.....")
+    print("...EEEE................BBB......BBB........OOO......OOO.....")
+    print("...EEEEEEEEEEEE........BBBBBBBBBBB.........OOO......OOO.....")
+    print("...EEEEEEEEEEEE........BBBBBBBBBBB.........OOO......OOO.....")
+    print("...EEEE................BBB......BBB........OOO......OOO.....")
+    print("...EEEE................BBB......BBB........OOO......OOO.....")
+    print("...EEEEEEEEEEEE...**...BBBBBBBBBBBB...**...OOOOOOOOOOOO...**")
+    print("...EEEEEEEEEEEE...**...BBBBBBBBBBBB...**...OOOOOOOOOOOO...**")
+    print()
 
 
 main()
